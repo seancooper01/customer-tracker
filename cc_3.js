0ecff7731109cc047bb4027a994bf6f01b1e6402
Sean Cooper <seancooper@usf.edu>
2025-09-13 18:35:01 -0400

    step 2 done

--- cc_3.js
+++ cc_3.js
@@ -1,3 +1,22 @@
 // Coding Challenge 00
 
+// Step 1
+
+let customers = [
+    {   name: "Bob", 
+        email: "bobthebuilder@gmail.com", 
+        purchases: ["Hammer", "Wood", "Nails"] 
+    },
+    {
+        name: "John Brian Johnson",
+        email: "JBJ@gmail.com",
+        purchases: ["Shirts", "Pants", "Socks"]
+    },
+    {
+        name: "ElChapo",
+        email: "ELChapo@gmail.com",
+        purchases: ["Gunpowder", "BodyBag", "Gasoline"]
+    }
+]; 
+      //  console.log(customers); 
 // Code goes here
